ec625bb0f59930eb5f6b5c59c8bcfcedfe62102d
AngleAA <jaedritzangel@gmail.com>
2020-02-25 23:30:52 +0000

    some stuff

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let songs = ['Initial','KGI','MIL','FB','RIOT']
+
+let Initial = {
+    name: "Initial",
+    artist: "Poppin' Party",
+    length: ,
+    link: ,
+    img: 
+}
+
+let KGI = {
+    name: "I Need You!",
+    artist: "Hello Happy World",
+    length: ,
+    link: ,
+    img: 
+}
+
+let MIL = {
+    name: "Luminous Once More",
+    artist: "Pastel Palettes",
+    length: ,
+    link: ,
+    img: ,
+}
+
+let FB = {
+    name: "FIRE BIRD",
+    artist: "Roselia",
+    length: ,
+    link: ,
+    img: 
+}
+
+let RIOT = {
+    name: "R.I.O.T",
+    artist: "Raise A Suilen",
+    length: ,
+    link: ,
+    img: 
+}
